0fd00d3f63d5c8245c3877f21bfcce0ab4c4ef99
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-04-12 12:52:57 -0700

    working on part 2

--- tvmaze.js
+++ tvmaze.js
@@ -24,27 +24,48 @@ async function getShowsByTerm( /* term */) {
   let showData = await axios.get(`${BASE_URL}${SEARCH_ENDPOINT}`, {
     params: {q: 'Seinfeld'}
   });
-  console.log(showData)
 
-  return showData;
-  //  [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary:
-  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-  //          women with extraordinary skills that helped to end World War II.</p>
-  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-  //          normal lives, modestly setting aside the part they played in
-  //          producing crucial intelligence, which helped the Allies to victory
-  //          and shortened the war. When Susan discovers a hidden code behind an
-  //          unsolved murder she is met by skepticism from the police. She
-  //          quickly realises she can only begin to crack the murders and bring
-  //          the culprit to justice with her former friends.</p>`,
-  //     image:
-  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
+  // let showId = showData.data[0].show.id;
+  // let showName = showData.data[0].show.name;
+  // let showSummary = showData.data[0].show.summary;
+  // let showImage = showData.data[0].show.image.original;
+
+  //console.log([showId, showName, showSummary, showImage]);
+  //for(let how in showData.data){
+
+  //}
+  console.log(showData);
+  let result = showData.data.map(show => {
+    show{ id
+    id:show.id,
+    name:show.name,
+    summary:show.summary,
+    //image:show.image.original
+    }
+  })
+
+  console.log(result);
+
+
+
+   [
+    {
+      id: 1767,
+      name: "The Bletchley Circle",
+      summary:
+        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+           women with extraordinary skills that helped to end World War II.</p>
+         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+           normal lives, modestly setting aside the part they played in
+           producing crucial intelligence, which helped the Allies to victory
+           and shortened the war. When Susan discovers a hidden code behind an
+           unsolved murder she is met by skepticism from the police. She
+           quickly realises she can only begin to crack the murders and bring
+           the culprit to justice with her former friends.</p>`,
+      image:
+          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+    }
+  ]
 }
 
 
